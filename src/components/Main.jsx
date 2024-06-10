a9e17d8373c71ea61677961232b12253170feac6
Brunno Costa <sbrunno.costa@gmail.com>
2024-06-10 19:43:51 -0300

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -75,12 +75,16 @@ const Main = () => {
         const message = `
         Olá! Gostaria de fazer um pedido.
         Aqui está o produto que desejo:
-        🍕 Produto: ${product.title};
-        📝 Descrição: ${product.description};
-        🔢 Quantidade: ${product.quantity};
-        📏 Tamanho: ${product.size};
+
+        🍕 *Produto: ${product.title};
+        📝 *Descrição: ${product.description};
+        🔢 *Quantidade: ${product.quantity};
+        📏 *Tamanho: ${product.size};
+
         ℹ️ Por favor, informe o tempo estimado de entrega e o valor total.
-        😊 Muito obrigado!`;
+        
+        😊 Muito obrigado!`
+        ;
         const phone = '5584996492087'; // Coloque o número de telefone do estabelecimento aqui
         const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, '_blank');
