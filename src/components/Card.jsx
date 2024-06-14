2d064d92bf87ff90f8153a274a40d3904a71f01a
Brunno Costa <sbrunno.costa@gmail.com>
2024-06-13 21:01:24 -0300

    :sparkles: feat: add whasapp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,9 +16,8 @@ import Modal from 'react-modal';
 
 export function Card({ products }) {
 
-    const merda = [products]
+    const ArrayProducts = [products]
 
-    console.log(products);
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -74,8 +73,8 @@ export function Card({ products }) {
             size,
             quantity,
         };
-        // handleBuy(item)
-        console.log(product);
+        handleBuy(item)
+        console.log(item);
     }
 
     const openModal = () => {
@@ -106,86 +105,86 @@ export function Card({ products }) {
 
     return (
 
-        merda.map(product => {
+        ArrayProducts.map(product => {
 
             return (
-<>
-                <div className="product  h-full drop-shadow-2xl border rounded-t-xl rounded-b-md border-none">
-                    <div className='flex justify-center items-center'>
-                        <ImageWithDefaultSize src={product.img} className='rounded-t-xl' />
-                        {/* <img src={product.img} alt="" className='w-full h-full  object-cover rounded-t-xl' /> */}
-                    </div>
-                    <div className='flex-col leading-loose bg-gray-100  p-4 rounded-b-xl overflow-hidden '>
-                        <h1 className='text-xl text-[#B5121B] font-semibold' >{product.title}</h1>
-                        <p className=' text-sm text-[#B5121B]'>{product.description}</p>
-                        <div className='flex justify-between items-center'>
-                            <p className='text-xl font-bold text-[#B5121B]'>R$ {product.price}.00</p>
+                <>
+                    <div className="product  h-full drop-shadow-2xl border rounded-t-xl rounded-b-md border-none">
+                        <div className='flex justify-center items-center'>
+                            <ImageWithDefaultSize src={product.img} className='rounded-t-xl' />
+                            {/* <img src={product.img} alt="" className='w-full h-full  object-cover rounded-t-xl' /> */}
+                        </div>
+                        <div className='flex-col leading-loose bg-gray-100  p-4 rounded-b-xl overflow-hidden '>
+                            <h1 className='text-xl text-[#B5121B] font-semibold' >{product.title}</h1>
+                            <p className=' text-sm text-[#B5121B]'>{product.description}</p>
+                            <div className='flex justify-between items-center'>
+                                <p className='text-xl font-bold text-[#B5121B]'>R$ {product.price}.00</p>
 
-                            {/* <button onClick={() => handleBuy(product.title)}>
+                                {/* <button onClick={() => handleBuy(product.title)}>
 
                                                     <IoLogoWhatsapp size={'1.4rem'} fill='#25D366' />
                                                 </button> */}
+                            </div>
+
+                            <div className="flex flex-col py-2 gap-4 font-sans text-xl">
+                                <div className='flex gap-4 justify-between'>
+                                    <label className='text-center flex justify-center items-center font-bold' htmlFor="pizza-size">Tamanho:</label>
+                                    <select className='p-2' id="pizza-size " value={size} onChange={handleSizeChange}>
+                                        <option value="pequena">Pequena</option>
+                                        <option value="media">Média</option>
+                                        <option value="grande">Grande</option>
+                                        <option value="familia">Família</option>
+                                    </select>
+                                </div>
+                                <div className='flex gap-4 justify-between'>
+                                    <label className='py-2 font-bold' htmlFor="pizza-quantity">Quantidade:</label>
+                                    <input
+                                        className='w-1/6 px-2 rounded-md focus:border-[#B5121B] focus:outline-none focus:ring-2 focus:ring-[#B5121B]'
+                                        type="number" id="pizza-quantity"
+                                        value={quantity}
+                                        onChange={handleQuantityChange} />
+                                </div>
+                            </div>
+
+                            <div className='flex justify-center items-center p-2 '>
+                                <button
+                                    onClick={() => openModal()}
+                                    className='w-full p-2  bg-gray-200  rounded-full text-xl hover:bg-[#B5121B] hover:text-white active:bg-[#B5121B] focus:outline-none focus:ring focus:ring-[#e47e83] '>
+                                    Finalizar pedido
+                                </button>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col py-2 gap-4 font-sans text-xl">
-                            <div className='flex gap-4 justify-between'>
-                                <label className='text-center flex justify-center items-center font-bold' htmlFor="pizza-size">Tamanho:</label>
-                                <select className='p-2' id="pizza-size " value={size} onChange={handleSizeChange}>
-                                    <option value="pequena">Pequena</option>
-                                    <option value="media">Média</option>
-                                    <option value="grande">Grande</option>
-                                    <option value="familia">Família</option>
-                                </select>
-                            </div>
-                            <div className='flex gap-4 justify-between'>
-                                <label className='py-2 font-bold' htmlFor="pizza-quantity">Quantidade:</label>
-                                <input
-                                    className='w-1/6 px-2 rounded-md focus:border-[#B5121B] focus:outline-none focus:ring-2 focus:ring-[#B5121B]'
-                                    type="number" id="pizza-quantity"
-                                    value={quantity}
-                                    onChange={handleQuantityChange} />
-                            </div>
-                        </div>
-
-                        <div className='flex justify-center items-center p-2 '>
-                            <button
-                                onClick={() => openModal()}
-                                className='w-full p-2  bg-gray-200  rounded-full text-xl hover:bg-[#B5121B] hover:text-white active:bg-[#B5121B] focus:outline-none focus:ring focus:ring-[#e47e83] '>
-                                Finalizar pedido
-                            </button>
-                        </div>
                     </div>
 
-                </div>
-
-                <Modal
-                                            isOpen={modalIsOpen}
-                                            onRequestClose={closeModal}
-                                            contentLabel="Enviar o pedido?"
-                                            className="fixed inset-0 flex items-center justify-center p-4 bg-opacity-75"
-                                            overlayClassName="fixed inset-0 bg-opacity-50"
-                                        >
-                                            <div className="bg-white rounded-lg p-6 max-w-xl mx-auto flex flex-col">
-                                                <h2 className="text-2xl font-bold mb-4">📝 Pedido</h2>
-                                                <p className="mb-4">Deseja enviar o pedido?</p>
-                                                <ol className='flex flex-col px-2'>
-                                                    <li>✅ Quantidade: {quantity}</li>
-                                                    <li>✅ Tamanho: {size}</li>
-                                                    <li>✅ Titulo: {product.title}</li>
-                                                    <li>✅ Preço: {product.price}</li>
-                                                </ol>
-                                                <div className='flex justify-center items-center gap-6 mt-4'>
-                                                    <button onClick={closeModal} className="px-4 py-2 bg-red-500 text-white rounded">
-                                                        Fechar
-                                                    </button>
-                                                    <button onClick={() => (addToCart(product))} className="px-4 py-2 bg-red-500 text-white rounded">
-                                                        Enviar
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </Modal>
+                    <Modal
+                        isOpen={modalIsOpen}
+                        onRequestClose={closeModal}
+                        contentLabel="Enviar o pedido?"
+                        className="fixed inset-0 flex items-center justify-center p-4 bg-opacity-75"
+                        overlayClassName="fixed inset-0 bg-opacity-50"
+                    >
+                        <div className="bg-white rounded-lg p-6 max-w-xl mx-auto flex flex-col">
+                            <h2 className="text-2xl font-bold mb-4">📝 Pedido</h2>
+                            <p className="mb-4">Deseja enviar o pedido?</p>
+                            <ol className='flex flex-col px-2'>
+                                <li>✅ Quantidade: {quantity}</li>
+                                <li>✅ Tamanho: {size}</li>
+                                <li>✅ Titulo: {product.title}</li>
+                                <li>✅ Preço: {product.price}</li>
+                            </ol>
+                            <div className='flex justify-center items-center gap-6 mt-4'>
+                                <button onClick={closeModal} className="px-4 py-2 bg-red-500 text-white rounded">
+                                    Fechar
+                                </button>
+                                <button onClick={() => (addToCart(product))} className="px-4 py-2 bg-red-500 text-white rounded">
+                                    Enviar
+                                </button>
+                            </div>
+                        </div>
+                    </Modal>
                 </>
-                
+
             )
 
         })
